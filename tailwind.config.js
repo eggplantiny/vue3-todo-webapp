f39be2263a684973c396daae14c883d4f31de534
eggplantiny <eggplantiny@gmail.com>
2021-12-11 01:46:02 +0900

    modify tailwind confing to set mode 'jit'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  mode: 'jit',
+  purge: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx,scss}'
+  ],
   whitelist: ['bg-white', 'bg-transparent', 'opacity-0', 'opacity-100', 'transition-all'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
